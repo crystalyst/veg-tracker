3ba16dd50d925d3f9b37ed8859fbba818a33229e
Je Seung You (Jason) <jy2908@columbia.edu>
2024-10-04 10:54:24 -0400

    [미션2] 깃허브 리포지토리에 프로젝트 올리기

---
+++ src/main/kotlin/com/example/veg_tracker/VegTrackerApplication.kt
@@ -0,0 +1,11 @@
+package com.example.veg_tracker
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class VegTrackerApplication
+
+fun main(args: Array<String>) {
+	runApplication<VegTrackerApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/veg_tracker/VegTrackerApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.veg_tracker
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class VegTrackerApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
